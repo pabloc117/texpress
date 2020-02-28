a7ad9e24e63f68e9fa1c6910e1145554c6f8fcba
Pablo Cifuentes <pablo11cifuentes@gmail.com>
2020-02-28 16:20:13 -0600

    Template engine

---
+++ template-engine/express-jsx.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+function getKeysFromOptions(options) {
+    const {setting, _locals, ...objectKeys} =options;
+    return Object.keys(objectKeys);
+}
+
+function getRenderedContent(content, options) {
+    const keys = getKeysFromOptions(options);
+    let contentString = content.toString();
+
+    for (let key of keys) {
+        contentString = contentString.replace(
+            new RegExp(`\{${key}\}`,"gi"), 
+            options[key]
+            );
+        }
+    return contentString;
+}
+function expressJxs(filePath, options, callback) {
+    fs.readFile(filePath, function (err,content) {
+       if (err) {
+       return callback(null, rendered); 
+       }
+       const rendered = getRenderedContent(content, options);
+       return callback(null, rendered);
+    });
+}
+
+module.exports = expressJxs;
